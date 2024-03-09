4a7d850f3377bafc55507db02b763f54c18ab4ca
ndbhung1810 <lehoang21998@gmail.com>
2024-03-09 13:55:30 +0700

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-const express = require("express")
-const app = express()
-var a= 1;
+const PORT = process.env.PORT || 8000
+const hostname = 'localhost'
 
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-
-app.use(express.urlencoded({extended: false}))
+const app = express()
 app.use(express.json())
-
-
-const cors = require("cors")
 app.use(cors())
 
-const productRouter = require('./routes/product.router')
-
-app.use("/api/products", productRouter)
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log("Server is running....")
+const API_KEY = process.env.API_KEY
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: req.body.message}],
+            max_tokens: 4000,
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
 })
+
+app.listen(PORT, () => console.log(`Your server running on http://${hostname}:${PORT}`));
